5813d44a3e11b63b62afe79c2307d2f6733ba587
nlokam99 <42047109+nlokam99@users.noreply.github.com>
2018-08-02 17:24:42 -0400

    Update OpenCV_install.sh

--- OpenCV_install.sh
+++ OpenCV_install.sh
@@ -28,7 +28,7 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE \
     -D CMAKE_INSTALL_PREFIX=/usr/local \
     -D INSTALL_C_EXAMPLES=OFF \
     -D INSTALL_PYTHON_EXAMPLES=ON \
-    -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
+    -D OPENCV_EXTRA_MODULES_PATH=~/opencv/opencv_contrib/modules \
     -D BUILD_EXAMPLES=ON ..
 make -j4
 sudo make install
